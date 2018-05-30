ce8586f998d72c7dcc189419a15ed1f5e4f4b8b0
zouxindesign <18888179739@163.com>
2018-05-30 16:57:55 +0800

    a

--- amazing_stuff/src/components/add_needs/needs.vue
+++ amazing_stuff/src/components/add_needs/needs.vue
@@ -12,35 +12,45 @@
     <el-button plain @click="onSubmit">点击求片哟~</el-button>
   </div>
 </template>
-  
+
 <script>
-    export default {
-      data() {
-        return {
-          need_film: {
-            name: ''
-          }
-        };
-      },
-      methods:{
-        onSubmit(){
-          console.log(this.need_film.name)
+  export default {
+    data() {
+      return {
+        need_film: {
+          name: ''
         }
+      };
+    },
+    methods: {
+      onSubmit() {
+        let needFilms = Bmob.Object.extend("needFilms");
+        let needfilms = new needFilms();
+        needfilms.save(this.need_film, {
+          success: function (needfilms) {
+            console.log(needfilms)
+          },
+          error: function (model, error) {
+            console.log(error)
+            console.log(model)
+          }
+        })
       }
     }
-  </script>
+  }
+</script>
 
 
-  
-  <!-- Add "scoped" attribute to limit CSS to this component only -->
-  <style scoped>
-    .box-card{
-      width: 90%;
-      margin: 50% auto 50px auto;
-    }
-    .el-form{
-      margin: 15px;
-      text-align: center;
-    }
-  </style>
-  
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .box-card {
+    width: 90%;
+    margin: 50% auto 50px auto;
+  }
+
+  .el-form {
+    margin: 15px;
+    text-align: center;
+  }
+</style>
